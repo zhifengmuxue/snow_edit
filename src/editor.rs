48cd9b3cb73f37cc77289136ddb1ffc6792fac07
lima <zhifengmuxue@163.com>
2025-03-30 22:26:08 +0800

    feat: 版本迭代

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,15 @@
 mod terminal;
 
-use crossterm::event::{read, Event::{self, Key}, KeyCode::Char, KeyEvent, KeyModifiers};
-use std::io::Error;
-use terminal::{Terminal, Size, Position};
+use crossterm::event::{
+    Event::{self, Key},
+    KeyCode::Char,
+    KeyEvent, KeyModifiers, read,
+};
+use std::{env, io::Error};
+use terminal::{Position, Size, Terminal};
+
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
@@ -25,7 +32,7 @@ impl Editor {
     fn repl(&mut self) -> Result<(), Error> {
         loop {
             self.refresh_screen()?;
-            if self.should_quit{
+            if self.should_quit {
                 break;
             }
             let event = read()?;
@@ -35,12 +42,13 @@ impl Editor {
     }
 
     /// 按键事件
-    fn evaluate_event(&mut self, event: &Event){
-        if let Key(KeyEvent{
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
             code, modifiers, ..
-        }) = event{
+        }) = event
+        {
             match code {
-                Char('d') if *modifiers == KeyModifiers::CONTROL =>{
+                Char('d') if *modifiers == KeyModifiers::CONTROL => {
                     self.should_quit = true;
                 }
                 _ => (),
@@ -51,10 +59,10 @@ impl Editor {
     /// 刷新屏幕
     fn refresh_screen(&self) -> Result<(), Error> {
         Terminal::hide_cursor()?;
-        if self.should_quit{
+        if self.should_quit {
             Terminal::clear_screen()?;
             Terminal::print("Goodbye.\r\n")?;
-        }else{
+        } else {
             Self::draw_rows()?;
             Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
         }
@@ -63,13 +71,36 @@ impl Editor {
         Ok(())
     }
 
+    fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{NAME} - version {VERSION}\r\n");
+        let width = Terminal::size()?.width;
+        let len = welcome_message.len();
+        #[allow(clippy::integer_division)]
+        let padding = (width.saturating_sub(len)) / 2;
+        let space = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("{space}{welcome_message}");
+        welcome_message.truncate(width);
+        Terminal::print(welcome_message)?;
+        Ok(())
+    }
+
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+
     /// 绘制‘～’标识
-    fn draw_rows() -> Result<(), Error>{
-        let Size{height, ..} = Terminal::size()?;
-        for current_row in 0..height{
+    fn draw_rows() -> Result<(), Error> {
+        let Size { height, .. } = Terminal::size()?;
+        for current_row in 0..height {
             Terminal::clear_line()?;
-            Terminal::print("~")?;
-            if current_row + 1 < height{
+            #[allow(clippy::integer_division)]
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+            if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
         }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,88 +1,99 @@
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
 use crossterm::cursor::{Hide, MoveTo, Show};
 use crossterm::style::Print;
-use crossterm::queue;
-use std::io::{stdout, Write, Error};
+use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size};
+use crossterm::{Command, queue};
+use std::fmt::Display;
+use std::io::{Error, Write, stdout};
 
 /// 大小
 #[derive(Clone, Copy)]
-pub struct Size{
-    pub height: u16,
-    pub width: u16,
+pub struct Size {
+    pub height: usize,
+    pub width: usize,
 }
 
 /// 位置
 #[derive(Clone, Copy)]
-pub struct Position{
-    pub x: u16,
-    pub y: u16,
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
 }
 
 /// 终端行为
 pub struct Terminal;
 
-impl Terminal{
+impl Terminal {
     /// 终止终端
-    pub fn terminate() -> Result<(), Error>{
+    pub fn terminate() -> Result<(), Error> {
         Self::execute()?;
         disable_raw_mode()?;
         Ok(())
     }
 
     /// 初始化
-    pub fn initialize() -> Result<(), Error>{
+    pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(Position{x:0, y:0})?;
+        Self::move_cursor_to(Position { x: 0, y: 0 })?;
         Self::execute()?;
         Ok(())
     }
 
     /// 清理屏幕
-    pub fn clear_screen() -> Result<(), Error>{
-        queue!(stdout(), Clear(ClearType::All))?;
+    pub fn clear_screen() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
 
     /// 清理一行
-    pub fn clear_line() -> Result<(), Error>{
-        queue!(stdout(), Clear(ClearType::CurrentLine))?;
+    pub fn clear_line() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
     /// 移动光标
-    pub fn move_cursor_to(pos: Position) -> Result<(), Error>{
-        queue!(stdout(), MoveTo(pos.x, pos.y))?;
+    pub fn move_cursor_to(pos: Position) -> Result<(), Error> {
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+        Self::queue_command(MoveTo(pos.x as u16, pos.y as u16))?;
         Ok(())
     }
 
     /// 隐藏光标
-    pub fn hide_cursor() -> Result<(),Error>{
-        queue!(stdout(), Hide)?;
+    pub fn hide_cursor() -> Result<(), Error> {
+        Self::queue_command(Hide)?;
         Ok(())
     }
 
     /// 展示光标
-    pub fn show_cursor() -> Result<(), Error>{
-        queue!(stdout(), Show)?;
+    pub fn show_cursor() -> Result<(), Error> {
+        Self::queue_command(Show)?;
         Ok(())
     }
 
     /// 输出
-    pub fn print(string: &str) -> Result<(), Error>{
-        queue!(stdout(), Print(string))?;
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
         Ok(())
     }
 
     /// 大小
-    pub fn size() -> Result<Size, Error>{
-        let (width, height) = size()?;
-        Ok(Size { height, width})
+    pub fn size() -> Result<Size, Error> {
+        let (width_u16, height_u16) = size()?;
+        #[allow(clippy::as_conversions)]
+        let height = height_u16 as usize;
+        #[allow(clippy::as_conversions)]
+        let width = width_u16 as usize;
+        Ok(Size { height, width })
     }
 
     /// 执行
-    pub fn execute() -> Result<(),Error>{
+    pub fn execute() -> Result<(), Error> {
         stdout().flush()?;
+        Ok(())
+    }
+
+    fn queue_command(command: impl Command) -> Result<(), Error> {
+        queue!(stdout(), command)?;
         Ok(())
     }
 }
